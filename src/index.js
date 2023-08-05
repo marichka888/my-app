6a3e7902fd0b582aa2bd494ff0c879588bd23cce
marichka888 <mariatomchuk11@gmail.com>
2023-08-05 13:46:45 +0300

    Create index.js
    added JS

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function FormatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${days[day]} ${hours}:${minutes}`;
+}
+
+let currentDate = document.querySelector("#date");
+let currentTime = new Date();
+
+currentDate.innerHTML = `${days[day]} ${hours}:${minutes}`;
